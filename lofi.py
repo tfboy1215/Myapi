d6167ee62e1a76e3b09213fa2f52ed42a8c8ad72
tfboy1215 <panatda.pho@ku.th>
2022-05-13 23:22:37 +0700

    finding area

--- lofi.py
+++ lofi.py
@@ -1,7 +1,37 @@
-a = 17.5
-b = 35
-c = 20
-print(a+b+c)
-print(a-b+c)
-print(a+b*c)
-print(a/b)
+# a = 17.5
+# b = 35
+# c = 20
+# print(a+b+c)
+# print(a-b+c)
+# print(a+b*c)
+# print(a/b)
+
+# x=float(input("Q:"))
+# y=float(input("A:"))
+# print(x)
+# print(y)
+# print(x/y)
+
+# x = float(input("width: ")) //หาพื้นที่สี่เหลี่ยม
+# y = float(input("height: "))
+# z = x*y
+# print(z)
+
+# r=float(input(""))  //หาพื้นที่วงกลม
+# z = 3.14*r*r
+# print(z)
+
+# a=float(input("สูง"))  //หาพื้นที่สามเหลี่ยม
+# b=float(input("ฐาน"))
+# x = 0.5*a*b
+# print(x)
+
+# r=float(input("r"))   //หาพื้นที่ทรงกระบอก
+# h=float(input("h"))
+# t=3.14*r*r*h
+# print(t)
+
+c=float(input("width"))
+f=float(input("long"))
+k=c*f
+print(k)
